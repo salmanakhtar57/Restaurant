68597f3ee103729aae588fc2177921d112c4db56
salmanakhtar57 <salmanmaken57@gmail.com>
2023-12-24 22:32:28 +0500

    Queries: Add data and write queries

--- myrestaurant/inventory/admin.py
+++ myrestaurant/inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Ingredient, RecipeRequirement, MenuItem, Purchase
 # Register your models here.
+
+admin.site.register(Ingredient)
+admin.site.register(MenuItem)
+admin.site.register(RecipeRequirement)
+admin.site.register(Purchase)
